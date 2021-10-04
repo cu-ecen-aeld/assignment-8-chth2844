07bfeed0ae68b5183330ff6ae9d9cf5e3c82f28d
chth2844 <chth2844@colorado.edu>
2021-10-03 21:42:39 -0600

    fulltest passing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd8b48627aaf18c151bbf6d1ac5789dc2e59c0268'
+AESD_ASSIGNMENTS_VERSION = '78eeda32f625adae9f6799bde41b8cc01937936b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
